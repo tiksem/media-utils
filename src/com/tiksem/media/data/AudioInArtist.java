548101918a76db38917a54200196ab6924004693
Semyon Tikhonenko <semyontikhonenko@gmail.com>
2014-10-17 16:26:03 +0300

    AudioInArtist extends Audio

--- src/com/tiksem/media/data/AudioInArtist.java
+++ src/com/tiksem/media/data/AudioInArtist.java
@@ -3,5 +3,8 @@ package com.tiksem.media.data;
 /**
  * Created by CM on 8/29/2014.
  */
-public final class AudioInArtist {
+public abstract class AudioInArtist extends Audio {
+    private AudioInArtist(Audio other) {
+        super(other);
+    }
 }
