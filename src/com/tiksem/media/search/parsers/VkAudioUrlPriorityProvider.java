824e0120fc753432f5e485a53872397586fd70bc
Semen Tykhonenko <stykhonenko@cogniance.com>
2015-11-20 10:55:01 +0200

    containMinus инструментал added

--- src/com/tiksem/media/search/parsers/VkAudioUrlPriorityProvider.java
+++ src/com/tiksem/media/search/parsers/VkAudioUrlPriorityProvider.java
@@ -182,7 +182,7 @@ public class VkAudioUrlPriorityProvider implements CollectionUtils.PrioritiesPro
     private boolean containsMinus(String input) {
         return input.contains("instrumental") || input.contains("minus") || input.contains("минус")
                 || input.contains("минусовка") || input.contains("instumental") || input.contains("guitar only") ||
-                input.contains("без слов") || input.contains("задавка");
+                input.contains("без слов") || input.contains("задавка") || input.contains("инструментал");
     }
 
     private boolean containsRemix(String input) {
